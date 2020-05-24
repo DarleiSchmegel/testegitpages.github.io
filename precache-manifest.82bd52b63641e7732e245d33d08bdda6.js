fff40ba7f58a68926c6bbef817afa311e799b46c
Darlei Schmegel <darleischmegel94@gmail.com>
2020-05-24 00:46:53 -0300

    Updates

---
+++ precache-manifest.82bd52b63641e7732e245d33d08bdda6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae240d90dbcd482cc7760fd37636e129",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06fc122b420cf4f997c8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "72b59a089c1375b0c198",
+    "url": "/static/js/2.18e09c51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.18e09c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06fc122b420cf4f997c8",
+    "url": "/static/js/main.4104e619.chunk.js"
+  },
+  {
+    "revision": "a030740e135e76608622",
+    "url": "/static/js/runtime-main.cba2cd54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
